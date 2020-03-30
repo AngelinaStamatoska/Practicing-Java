9c7b336710c63a83db21a958fff306bac6d29127
AngelinaStamatoska <angelinastamatoska@gmail.com>
2020-03-30 16:56:42 +0200

    Practicing with methods and objects

---
+++ AccessMethodsWithObjects/src/com/angelina/Car.java
@@ -0,0 +1,20 @@
+package com.angelina;
+
+//Create a class Car
+
+public class Car{
+
+    //Create a fullThrottle() method
+
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can");
+    }
+
+    //Create a speed() method and add a parameter
+
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+
+    }
+
+}
---
+++ AccessMethodsWithObjects/src/com/angelina/Main.java
@@ -0,0 +1,17 @@
+package com.angelina;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //First create myCar object
+        //Call fullThrottle() method
+        // Call speed() method
+
+        Car myCar = new Car();
+        myCar.fullThrottle();
+        myCar.speed(200);
+
+    }
+}
